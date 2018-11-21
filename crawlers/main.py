c81860c94d07a4e676d8a8d51c592e97ac617dff
shencp <15858127547@139.com>
2018-11-21 22:17:20 +0800

    2018-11-21

--- crawlers/main.py
+++ crawlers/main.py
@@ -3,6 +3,8 @@
 import requests
 
 r = requests.get('https://book.douban.com/subject/1084336/comments/')
+r.encoding ='utf-8'
+
 
 # ------------------------------------------------------------------------------------------------------
 # @ decode data
@@ -13,3 +15,13 @@ pattern = soup.find_all('span', 'short')
 for item in pattern:
     print('----')
     print(item.string)
+
+# ------------------------------------------------------------------------------------------------------
+# @ save data
+import pandas
+
+comments = []
+for item in pattern:
+    comments.append(item.string)
+df = pandas.DataFrame(comments)
+df.to_csv('comments.csv')
